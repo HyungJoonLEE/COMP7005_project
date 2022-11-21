553293cdb43248a4ff1a487a6b99d2e575619151
HyungJoon <itineris24@naver.com>
2022-11-21 02:28:44 -0800

    python function sample added

--- python_src/main.py
+++ python_src/main.py
@@ -1,25 +1,44 @@
-
-
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def draw_graph(value_list, size):
+    x = np.arange(0, len(value_list), 1)
+    y_value = []
+    temp = 0
 
+    for val in value_list:
+        if val == '1':
+            temp += 1
+            y_value.append(temp)
+        else:
+            y_value.append(temp)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    x = np.arange(1, 10, 0.1)
-    y = x * 0.2
-    y2 = np.sin(x)
-
-    plt.plot(x, y, 'b', label='first')
-    plt.plot(x, y2, 'r', label='second')
-    plt.xlabel('x axis')
-    plt.ylabel('y axis')
-    plt.title('matplotlib sample')
-    plt.legend(loc='upper right')
+    plt.plot(x, y_value, 'b', label='first')
+    # plt.legend(loc='upper right')
     plt.show()
+
+
+def process_file(file_pointer):
+    value_string = file_pointer.read()
+    print(list(value_string))
+    draw_graph(list(value_string), len(value_string))
+
+
+if __name__ == '__main__':
+    sender_packet_send = open("../cmake-build-debug/sender_packet_send.txt")
+    sender_ack_receive = open("../cmake-build-debug/sender_ack_receive.txt")
+    proxy_packet_send = open("../cmake-build-debug/proxy_packet_send.txt")
+    proxy_ack_send = open("../cmake-build-debug/proxy_ack_send.txt")
+    receiver_packet_receive = open(
+        "../cmake-build-debug/receiver_packet_receive.txt")
+
+    process_file(sender_packet_send)
+    process_file(sender_ack_receive)
+    process_file(sender_ack_receive)
+
+
+
+    # plt.plot(x, y, 'b', label='first')
+    # plt.plot(x, y2, 'r', label='second')
 
